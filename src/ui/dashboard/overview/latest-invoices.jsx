a5dcb0761965cff49ed8e622d3163d7a4367f648
Hest <hestdatabase@gmail.com>
2025-05-28 11:53:31 -0300

    Seção de últimas dívidas estilizada

--- src/ui/dashboard/overview/latest-invoices.jsx
+++ src/ui/dashboard/overview/latest-invoices.jsx
@@ -1,21 +1,22 @@
 import Image from "next/image"
 import { getLatestInvoices } from "@/lib/db/data"
+import { lusitanaFont } from "@/ui/fonts"
 
-export default async function LatestInvoices() {
+export default async function LatestInvoices({ styles }) {
   const latestInvoices = await getLatestInvoices()
   
   return (
-    <section>
+    <section className={styles}>
       {latestInvoices.map(invoice => (
         <div key={invoice.id}>
           <Image
           src={invoice.avatar}
           alt={invoice.name}
-          width={50}
-          height={50}
+          width={45}
+          height={45}
           />
           <h4>{invoice.name}</h4>
-          <span>{invoice.amount}</span>
+          <span className={lusitanaFont.className}>R${invoice.amount.toFixed(2)}</span>
         </div>
       ))}
     </section>
